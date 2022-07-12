a18f61a9b13a125dccd10e0c23f299979640d90e
Andrew Lee <alee68@fordham.edu>
2022-07-11 22:45:44 -0400

    finished model relations

--- models/index.js
+++ models/index.js
@@ -8,13 +8,17 @@ const ProductTag = require("./ProductTag");
 Product.belongsTo(Category, {
 	foreignKey: "category_id",
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
 	foreignKey: "category_id",
 });
+
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
 	Product,
